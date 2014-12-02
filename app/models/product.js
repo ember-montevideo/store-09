c3b5bd5de0848966e8dfb3a4446f36c671935499
Adrian Mugnolo <adrian@wyeworks.com>
2014-12-02 20:55:47 -0200

    Show each product

--- app/models/product.js
+++ app/models/product.js
@@ -4,5 +4,12 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
   price: DS.attr('number'),
-  isFeatured: DS.attr('boolean')
+  isFeatured: DS.attr('boolean'),
+
+  purchasePrice: function() {
+    return this.get('price') * (this.get('isFeatured') ? 0.8 : 1.0);
+  }.property('price'),
+  imageUrl: function() {
+    return "/images/" + this.get('id') + ".png";
+  }.property()
 });
--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('products');
+  this.resource('products', function() {
+    this.route('show', { path: ":product_id" });
+  });
 });
 
 export default Router;
