fd8fd0faae4f209d00d80e18b0bfef9e9c8f11fe
Adrian Mugnolo <adrian@wyeworks.com>
2015-01-08 21:57:53 -0200

    Demo nested query param reset

--- app/routes/products/show.js
+++ app/routes/products/show.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find("product", params.product_id);
+  },
+  setupController: function(controller, model) {
+    this.controllerFor('products').set('category', null);
+    controller.set('page', null);
+    this._super(controller, model);
   }
 });
